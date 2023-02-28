0d04e220d53ce5703339fcc789f30148aed127a0
Iceayy <117213186+Iceayydev@users.noreply.github.com>
2023-02-28 11:47:03 -0500

    Create slang.lua

---
+++ slang.lua
@@ -0,0 +1,32 @@
+local finna = ==
+local sussin = if
+local yeet = return
+local bussin = for
+local tweakin = while
+local be = =
+local bouta = +
+local cap = 0
+local but = else
+local bruh = then
+local aint = ~=
+local rob = -
+local mf = 1
+local showme = print
+local yikes = end
+local foHunnedGrand = 400000
+local ifAint = elseif
+
+function math()
+    sussin cap aint mf bruh
+    showme("who asked") but
+        yeet cap
+    yikes
+    sussin cap finna 0 bruh
+    showme("them hands")
+    yikes
+    sussin mf finna 1
+    mf be cap rob mf foHunnedGrand ifAint cap finna mf bruh
+    showme(mf)
+    yikes
+yikes
+
